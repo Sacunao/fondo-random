819c392af74c98f13493d5a3f9a8f03a8a9b757b
sacunao <sheila.aobregon@gmail.com>
2016-09-07 12:44:49 -0500

    aumento de funciones de intervalo con funcion callback

--- js/app.js
+++ js/app.js
@@ -1,27 +1,13 @@
-/*window.addEventListener("load", function() {
-    var boton = document.getElementById("calcular");
-    boton.addEventListener("click", function() {
-        intervalo (numeroInicial, segundos);
-    });
-
-        function intervalo (numeroInicial, segundos){
-        	setInterval(function(){
-        	document.getElementById("resultado").innerHTML = numeroInicial++; }, segundos*1000);
-        	document.getElementById("segundos").value = "";
-			document.getElementById("numeroInicial").value = "";
-        }
-});*/
-
 window.addEventListener("load", function() {
     var boton = document.getElementById("nuevoFondo");
         boton.addEventListener("click", function(){
-            var colorhexa = '#'+Math.floor(Math.random()*16777215).toString(16);
-            setInterval(callback);
+            var segundos = (parseInt(document.getElementById("segundos").value))*1000;
+            setInterval(callback, segundos);
     });
 
-    function color(colorhexa){
-        setTimeout(function(){ 
-            document.body.style.backgroundColor = colorhexa;}, 2000);
+    function callback(){
+            var colorhexa = '#'+Math.floor(Math.random()*16777215).toString(16);
+            document.body.style.backgroundColor = colorhexa;
     }
 
     
